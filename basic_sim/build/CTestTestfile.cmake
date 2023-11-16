9b41be0afa14d352084d753dd473c3b7b178e9f3
PapaVeneti <michael.d.papadakis@gmail.com>
2023-11-16 11:16:11 +0200

    removed tracking of local build files

--- basic_sim/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/papaveneti/rosDrake/src/basic_sim
-# Build directory: /home/papaveneti/rosDrake/src/basic_sim/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
