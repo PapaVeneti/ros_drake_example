98d803cdf14a76cad712490c21917ccabb552885
PapaVeneti <michael.d.papadakis@gmail.com>
2023-11-16 14:45:11 +0200

    Created controller node

--- basic_sim/src/controller_node.cpp
+++ basic_sim/src/controller_node.cpp
@@ -1,32 +1,54 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "std_msgs/Float64.h"
+#include "sensor_msgs/JointState.h"
 
-#include <sstream>
 
+class controller {
+public:
+    controller() : xd(0.0), n(), controller_interface(){
+        controller_interface = n.subscribe("/goal_set", 1000, &controller::interfaceCallback, this);
+        encoder_sub          = n.subscribe("/joint_states", 1000, &controller::encoderCallback, this);
+        controller_pub       = n.advertise<std_msgs::Float64>("/joint1_position_controller", 1000,true); //latched publisher
+        ROS_INFO("[Controller]: Initialized");
 
+    }
+    
+    void encoderCallback(const sensor_msgs::JointStateConstPtr & JointState){
+        ROS_DEBUG("[Controller]: Pendulum position is: %.3lf", JointState->position[0]);
+    }
+
+    void interfaceCallback(const std_msgs::Float64::ConstPtr & setPoint){
+        xd = setPoint->data; 
+        ROS_INFO("[Controller]: New controller set point: %.3lf", xd);
+        publishCommand();
+    }
+
+    void publishCommand(){
+        joint1_command.data = xd ; 
+        controller_pub.publish(joint1_command);
+    }
+
+private:
+    double xd;
+    ros::NodeHandle n;
+    std_msgs::Float64 joint1_command;
+    ros::Subscriber controller_interface;
+    ros::Subscriber encoder_sub;
+    ros::Publisher controller_pub; 
+};
 
 int main(int argc, char **argv){
 
   ros::init(argc, argv, "Controller_placeholder");
-  
-  ros::NodeHandle n;
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  controller controller;
   ros::Rate loop_rate(10);
-  int count = 0;
+  
 
   while (ros::ok())
   {
-    std_msgs::String msg;
-
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-    ROS_INFO("%s", msg.data.c_str());
-    chatter_pub.publish(msg);
 
     ros::spinOnce();
     loop_rate.sleep();
-    ++count;
   }
   return 0;
 }
